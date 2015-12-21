3f052ed7e21067dce47056fd85e4dc4fdbfc525b
Li Huang <lhuang210@gmail.com>
2015-12-21 18:07:44 -0500

    Create LevelOneToTwentyFive and SolverWindow class

---
+++ src/projectEuler/LevelOneToTwentyFive.java
@@ -0,0 +1,10 @@
+package projectEuler;
+
+/**
+ * The class which contains the method that solves problem one through twenty five
+ * @author Li Huang
+ * @version 12.21.2015
+ */
+public class LevelOneToTwentyFive {
+
+}
---
+++ src/projectEuler/SolverWindow.java
@@ -0,0 +1,14 @@
+package projectEuler;
+
+/**
+ * Class which consists of the main methods that use to show the answer
+ * @author Li Huang
+ * @version 12.21.2015
+ */
+public class SolverWindow {
+    
+    public static void main (String[]   arg)
+    {
+        
+    }
+}
