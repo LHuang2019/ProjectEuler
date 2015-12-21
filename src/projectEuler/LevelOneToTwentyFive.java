f8156be0a76cace497bc9ea0a8e1b1347f248480
Li Huang <lhuang210@gmail.com>
2015-12-21 18:36:41 -0500

    Solved problem one

--- src/projectEuler/LevelOneToTwentyFive.java
+++ src/projectEuler/LevelOneToTwentyFive.java
@@ -6,5 +6,31 @@ package projectEuler;
  * @version 12.21.2015
  */
 public class LevelOneToTwentyFive {
-
+    
+    /**
+     * Method which solves problem one
+     * The method takes input of x, y, and top, and returns the sum of
+     *      all multiples of x or y that is below  top
+     * @param x the first number
+     * @param y the second number
+     * @param top the greatest number that x or y can multiply to
+     * @return the sum of multiple of x or y that is below top
+     */
+    public static int problemOne(int x, int y, int stop)
+    {
+        stop = stop - 1;
+        
+        int nOne = stop / x;
+        int nTwo = stop / y;
+        int nThree = stop / (x * y);
+        
+        int sumOne = (nOne * (nOne + 1)) / 2 * x;
+        int sumTwo = (nTwo * (nTwo + 1)) / 2 * y;
+        int sumThree = (nThree * (nThree + 1)) / 2 * (x * y);
+        
+        int outPut = sumOne + sumTwo - sumThree;
+        
+        return outPut;
+    }
+    
 }
--- src/projectEuler/SolverWindow.java
+++ src/projectEuler/SolverWindow.java
@@ -1,5 +1,5 @@
 package projectEuler;
-
+;
 /**
  * Class which consists of the main methods that use to show the answer
  * @author Li Huang
@@ -7,8 +7,12 @@ package projectEuler;
  */
 public class SolverWindow {
     
+    /**
+     * The main method that gives the answer by calling other methods
+     * @param arg the input argument
+     */
     public static void main (String[]   arg)
     {
-        
+        System.out.println(LevelOneToTwentyFive.problemOne(3, 5, 1000));
     }
 }
